3413036b528502cf2d92f491403c63d5811dad6f
dochitavasile1987 <dochitavasile1987@gmail.com>
2020-01-13 14:03:41 +0200

    upd

---
+++ UserPosts/UserPosts.Data/CommentsDataAccess.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using UserPosts.Domain;
+using UserPosts.Services;
+
+namespace UserPosts.Data
+{
+    public class CommentDataAccess : BaseDataAccess<Comment>, ICommentsRepository
+    {
+        public IList<Comment> GetCommentsByPostId(int id)
+        {
+            var list = this.GetAll();
+            return list.Where(x => x.PostId == id).ToList();
+        }
+
+        protected override string GetFile()
+        {
+            throw new NotImplementedException();
+        }
+
+
+    }
+}
---
+++ UserPosts/UserPosts.Services/ICommentsRepository.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using UserPosts.Domain;
+
+namespace UserPosts.Services
+{
+    public interface ICommentsRepository : IBaseRepository<Comment>
+    {
+        IList<Comment> GetCommentsByPostId(int id);
+    }
+}
